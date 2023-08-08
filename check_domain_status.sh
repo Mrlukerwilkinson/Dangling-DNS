41e8788b1904745bf6b1588db00c3db6047470a8
Mrlukerwilkinson <140768032+Mrlukerwilkinson@users.noreply.github.com>
2023-08-08 22:05:05 +1000

    Update check_domain_status.sh
    Modified to use a TXT file as input. Easier to use if a list of domains has been exported in a previous step.

--- check_domain_status.sh
+++ check_domain_status.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-domains=("example.com" "google.com")
+# Populate a txt file of domains you want to check - each domain should be on a separate line
+input_file="domain_list.txt"
+
+# Location of where the output file should be saved
 output_file="dns_results.csv"
 
 # Create or truncate the CSV file
 echo "Domain,Status" > "$output_file"
 
-for domain in "${domains[@]}"; do
+# Read domains from input file
+while IFS= read -r domain || [[ -n "$domain" ]]; do
     result=$(dig "$domain")
     status_line=$(echo "$result" | grep -E "status: [A-Z]+" | tail -1)
     
@@ -19,6 +23,7 @@ for domain in "${domains[@]}"; do
     elif [[ "$status_line" == *"REFUSED"* ]]; then
         status="REFUSED"
     else
+    # Add additional status codes if necessary
         status="Unknown status"
     fi
 
@@ -27,4 +32,4 @@ for domain in "${domains[@]}"; do
     
     # Display the result on the screen
     echo "$domain: $status"
-done
+done < "$input_file"
